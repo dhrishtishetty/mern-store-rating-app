7bbb0a37e4b9d865d327b7fb305aa0fd8a09bb1d
dhrishtishetty <dhrishtishetty10@gmail.com>
2026-07-19 19:35:06 +0530

    final project submission

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    withCredentials: true
+    baseURL: "http://localhost:5000/api"
 });
 
 api.interceptors.request.use(config => {
